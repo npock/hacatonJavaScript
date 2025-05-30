79045b019575441f516c4c03f7f08a545112ffbc
Ilia Proskurin <pilailiapro@gmail.ru>
2025-05-30 18:28:54 +0300

    add random background color

--- src/menu.js
+++ src/menu.js
@@ -1,55 +1,28 @@
-import {Menu} from './core/menu'
+import {Menu} from './core/menu';
+import { BackgroundModule } from './modules/background.module';
 
 export class ContextMenu extends Menu {
     open(){
         document.addEventListener("contextmenu", (event) => {
             event.preventDefault();
-            this.locationOfMenu(this.el,event)
+            this.locationOfMenu(this.el,event);
+            this.add()
             this.el.classList.add('open');
-            const item = document.querySelector('.menu-item');
-            if(!item){
-                this.listAdd();
-            }
         });
     }
     close(){
         this.el.classList.remove('open');
     }
     add() {
-    
+        const backgroundModule = new BackgroundModule('color','Случайный фон');
+        this.el.innerHTML = backgroundModule.toHTML();
+        backgroundModule.trigger();
     }
     locationOfMenu(thisEl,event){
         const x = event.clientX;
         const y = event.clientY;
         thisEl.style.left = `${x}px`;
         thisEl.style.top = `${y}px`;
-    }
-    listAdd(){
-        const listItem1 = document.createElement('li');
-        listItem1.textContent = 'Аналитика кликов';
-        listItem1.className = 'menu-item';
-
-        const listItem2 = document.createElement('li');
-        listItem2.textContent = 'Случайная фигура';
-        listItem2.className = 'menu-item';
-
-        const listItem3 = document.createElement('li');
-        listItem3.textContent = 'Таймер отсчета ';
-        listItem3.className = 'menu-item';
-
-        const listItem4 = document.createElement('li');
-        listItem4.textContent = 'Случайный звук';
-        listItem4.className = 'menu-item';
-
-        const listItem5 = document.createElement('li');
-        listItem5.textContent = 'Случайный фон';
-        listItem5.className = 'menu-item';
-
-        const listItem6 = document.createElement('li');
-        listItem6.textContent = 'Кастомное сообщение';
-        listItem6.className = 'menu-item';
-
-        this.el.append(listItem1,listItem2,listItem3,listItem4,listItem5,listItem6);
     }
 }
 const contextmenu = new ContextMenu('.menu')
--- src/modules/background.module.js
+++ src/modules/background.module.js
@@ -1,5 +1,20 @@
 import {Module} from '../core/module'
 
 export class BackgroundModule extends Module {
-
+    trigger(){
+        const $button = document.querySelector('[data-type="color"]');
+        if($button){
+            $button.addEventListener('click',(event)=>{
+            document.body.style.backgroundColor = this.getRandomColor();
+        });
+        };
+    }
+    getRandomColor(){
+        const letters = '0123456789ABCDEF';
+        let color = '#';
+        for (let i = 0; i < 6; i++) {
+            color += letters[Math.floor(Math.random() * 16)];
+        };
+        return color
+    }
 }
