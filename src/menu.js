2c1b1dc655583054311140b66abed2e3455a970b
Mapkoffka <mapkoffka@gmail.com>
2025-06-01 17:21:06 +0500

    Implemented random figure

--- src/menu.js
+++ src/menu.js
@@ -1,5 +1,26 @@
 import {Menu} from './core/menu'
+import RandomFigure from './modules/random-figure.js';
 
 export class ContextMenu extends Menu {
+    constructor(menuElement) {
+        super(menuElement);
+    }
 
+    add(name, moduleInstance, label) {
+        this.registerModule(name, moduleInstance);
+
+        const menuItem = document.createElement('li');
+        menuItem.textContent = label || name;
+        menuItem.dataset.module = name;
+
+        this.menuElement.appendChild(menuItem);
+    }
 }
+
+//Создаём экземпляр ContextMenu
+const menuElement = document.querySelector('#menu');
+const menu = new ContextMenu(menuElement);
+
+const randomFigureModule = new RandomFigure();
+
+menu.add('random-figure', randomFigureModule, 'Случайная фигура');
